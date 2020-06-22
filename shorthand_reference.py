b239512ca5ace29b65787606622c6f30e8bad46c
Adam395 <adam.ri.cohen@gmail.com>
2020-06-22 08:25:06 -0400

    Updated Aegis dictionary

---
+++ shorthand_reference.py
@@ -0,0 +1,36 @@
+hullsize = {
+    '1':'Single Seat',
+    '2':'Multi Crew',
+    '3':'Capital'
+}
+#--------------------------------------
+#Ship Classes Dict
+classes = {
+    'co':'Combat',
+    'm':'Mining',
+    's':'Salvage',
+    'h':'Hospital/Medical',
+    'g':'Gas Mining',
+    'e':'Exploration/Science',
+    't':'Passenger Transport/Touring',
+    'd':'Data Running',
+    'ca':'Cargo Transport',
+    're':'Repair',
+    'b':'Building/Construction',
+    'ra':'Racing',
+    'v':'Vendor/Market'
+}
+#--------------------------------------
+#Subclasses Dict
+subclasses = {
+    'f':'Fighter',
+    'b':'Bomber',
+    'i':'Interdictor',
+    'c':'Carrier',
+    'd':'Destroyer',
+    's':'Stealth',
+    'e':'EMP Enabled',
+    'q':'Quantum Dampener Enabled',
+    'p':'Personnel/Dropship',
+    'j':'Single Hangar Bay'
+}
